2878ef9c97e97f7351882934145e08e28a27fe1a
waihoyu <13701343809@qq.com>
2018-10-10 17:08:12 +0800

    update  webpack

---
+++ learnVue10/a.js
@@ -0,0 +1,11 @@
+console.log("1");
+
+export let str = '测试js'
+
+export let str2 = '运行js'
+
+//会将str和str2 放到一个对象内导出
+
+export  function f() {
+    alert('1')
+}
---
+++ learnVue10/main.js
@@ -0,0 +1,13 @@
+
+//在另一个文件中将内容解构出来即可使用
+
+import {str, str2,fn} from './a.js'
+
+console.log(str, str2);
+
+fn()
+
+// import  * as b from './a.js'
+//
+// export default  a
+// import  xxx from  './b.js'
